70bc524537964c519847631ab5a8ebc73db7332a
Stefan Langenbach <stefan@langenbach.online>
2022-12-16 18:19:21 +0100

    Add solution for distress signal

--- advent_of_code/twenty_twenty_two/day13.py
+++ advent_of_code/twenty_twenty_two/day13.py
@@ -1,10 +1,14 @@
 """Puzzle for advent of code 2022 day 13."""
 
 import ast
+import math
+from functools import cmp_to_key
 from typing import Union
 
 from advent_of_code.constants import TWENTY_TWENTY_TWO_INPUT_PATH
 from advent_of_code.utils import load_input
+
+DIVIDER_PACKETS = [[[2]], [[6]]]
 
 
 def parse_input(raw_input: list[str]) -> list[list]:
@@ -38,11 +42,12 @@ def compare_integers(left: int, right: int) -> Union[bool, None]:
 def compare_lists(left: list, right: list) -> Union[bool, None]:
     for left_value, right_value in zip(left, right):
 
+        # recursively compare items of current pair
         is_ordered = is_correct_order(left_value, right_value)
         if is_ordered is not None:
             return is_ordered
 
-    if len(left) <= len(right):
+    if len(left) < len(right):
         return True
     elif len(left) > len(right):
         return False
@@ -66,22 +71,53 @@ def solve_part_one(raw_input: list[str]) -> int:
 
     parsed_input = parse_input(raw_input)
 
-    for index, pair in enumerate(parsed_input):
+    for index, pair in enumerate(parsed_input, start=1):
         if is_correct_order(left=pair[0], right=pair[1]):
-            correct_order += index + 1
+            correct_order += index
 
     return correct_order
 
 
-def solve_part_two(raw_input: list[str]):
-    pass
+def parse_input_part_two(raw_input: list[str]) -> list[list]:
+    parsed_input = [
+        ast.literal_eval(line) for line in raw_input if line.startswith("[")
+    ]
+    parsed_input.extend(DIVIDER_PACKETS)
+
+    return parsed_input
+
+
+def order_input(parsed_input: list[list]) -> list[list]:
+    def comparator(a: list, b: list) -> int:
+        if is_correct_order(a, b):
+            return 1
+        elif not is_correct_order(a, b):
+            return -1
+        else:
+            return 0
+
+    return sorted(parsed_input, key=cmp_to_key(comparator), reverse=True)
+
+
+def get_divider_packet_indices(ordered_input: list) -> list[int]:
+    return [
+        ordered_input.index(divider_packet) + 1 for divider_packet in DIVIDER_PACKETS
+    ]
+
+
+def solve_part_two(raw_input: list[str]) -> int:
+    parsed_input = parse_input_part_two(raw_input)
+    ordered_input = order_input(parsed_input)
+    divider_packet_indices = get_divider_packet_indices(ordered_input)
+
+    return math.prod(divider_packet_indices)
 
 
 def solve_puzzle():
     puzzle_input_path = TWENTY_TWENTY_TWO_INPUT_PATH.joinpath("day13")
     puzzle_input = load_input(puzzle_input_path)
     solution_part_one = solve_part_one(puzzle_input)
-    solution_part_two = None
+    solution_part_two = solve_part_two(puzzle_input)
     print(
         "Advent of code 2022 day 13:",
         f"Solution for part one: {solution_part_one}",
--- tests/test_day13.py
+++ tests/test_day13.py
@@ -4,8 +4,11 @@ from advent_of_code.constants import TEST_INPUT_PATH
 from advent_of_code.twenty_twenty_two.day13 import (
     compare_integers,
     compare_lists,
+    get_divider_packet_indices,
     is_correct_order,
+    order_input,
     parse_input,
+    parse_input_part_two,
     solve_part_one,
     solve_part_two,
 )
@@ -15,12 +18,22 @@ DAY13_TEST_INPUT_PATH = TEST_INPUT_PATH.joinpath("day13")
 
 
 @pytest.fixture(scope="session")
-def day13_input():
+def raw_input():
     return load_input(DAY13_TEST_INPUT_PATH)
 
 
-def test_parse_input(day13_input):
-    assert parse_input(day13_input) == [
+@pytest.fixture(scope="session")
+def parsed_input_part_two(raw_input):
+    return parse_input_part_two(raw_input)
+
+
+@pytest.fixture(scope="session")
+def ordered_input(parsed_input_part_two):
+    return order_input(parsed_input_part_two)
+
+
+def test_parse_input(raw_input):
+    assert parse_input(raw_input) == [
         [[1, 1, 3, 1, 1], [1, 1, 5, 1, 1]],
         [[[1], [2, 3, 4]], [[1], 4]],
         [[9], [[8, 7, 6]]],
@@ -68,10 +81,59 @@ def test_is_correct_order(left, right, expected):
     assert is_correct_order(left, right) == expected
 
 
-def test_solve_part_one(day13_input):
-    assert solve_part_one(day13_input) == 13
+def test_solve_part_one(raw_input):
+    assert solve_part_one(raw_input) == 13
 
 
-@pytest.mark.skip(reason="Not yet implemented")
-def test_solve_part_two(day13_input):
-    pass
+def test_parse_input_part_two(raw_input):
+    assert parse_input_part_two(raw_input) == [
+        [1, 1, 3, 1, 1],
+        [1, 1, 5, 1, 1],
+        [[1], [2, 3, 4]],
+        [[1], 4],
+        [9],
+        [[8, 7, 6]],
+        [[4, 4], 4, 4],
+        [[4, 4], 4, 4, 4],
+        [7, 7, 7, 7],
+        [7, 7, 7],
+        [],
+        [3],
+        [[[]]],
+        [[]],
+        [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+        [1, [2, [3, [4, [5, 6, 0]]]], 8, 9],
+        [[2]],
+        [[6]],
+    ]
+
+
+def test_order_input(parsed_input_part_two):
+    assert order_input(parsed_input_part_two) == [
+        [],
+        [[]],
+        [[[]]],
+        [1, 1, 3, 1, 1],
+        [1, 1, 5, 1, 1],
+        [[1], [2, 3, 4]],
+        [1, [2, [3, [4, [5, 6, 0]]]], 8, 9],
+        [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+        [[1], 4],
+        [[2]],
+        [3],
+        [[4, 4], 4, 4],
+        [[4, 4], 4, 4, 4],
+        [[6]],
+        [7, 7, 7],
+        [7, 7, 7, 7],
+        [[8, 7, 6]],
+        [9],
+    ]
+
+
+def test_get_divider_packet_indices(ordered_input):
+    assert get_divider_packet_indices(ordered_input) == [10, 14]
+
+
+def test_solve_part_two(raw_input):
+    assert solve_part_two(raw_input) == 140
